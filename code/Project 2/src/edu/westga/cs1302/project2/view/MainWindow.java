1ffa52ab3ad1b6980ea3ec89354520fb0357d7d8
Jessica G Stuckey <js00547@my.westga.edu>
2024-10-30 23:01:25 -0400

    Implimented Task 1C
    Connected the Comparator classes to the MainWindow GUI. Added an onAction method for the sort comboBox.

--- code/Project 2/src/edu/westga/cs1302/project2/view/MainWindow.java
+++ code/Project 2/src/edu/westga/cs1302/project2/view/MainWindow.java
@@ -1,12 +1,22 @@
 package edu.westga.cs1302.project2.view;
 
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+
+import edu.westga.cs1302.project2.model.IngredientNameComparator;
+import edu.westga.cs1302.project2.model.IngredientTypeComparator;
 import edu.westga.cs1302.project2.model.Ingredient;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.collections.transformation.SortedList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Alert;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.ListView;
 import javafx.scene.control.TextField;
+import javafx.scene.control.Alert.AlertType;
 
 /**
  * Codebehind for the Main Window of the application.
@@ -18,6 +28,7 @@ public class MainWindow {
 	@FXML private ComboBox<String> ingredientType;
 	@FXML private ListView<Ingredient> ingredientsList;
 	@FXML private TextField ingredientName;
+    @FXML private ComboBox<Comparator<Ingredient>> sortComboBox;
 
 	@FXML
 	void addIngredient(ActionEvent event) {
@@ -40,7 +51,22 @@ public class MainWindow {
 			this.ingredientsList.getItems().remove(selectedIngredient);
 		}
 	}
-
+	
+	  @FXML
+	    void changeSort(ActionEvent event) {
+		  if (this.sortComboBox.getValue().toString().equals("Name")) {
+			  ObservableList<Ingredient> ingredients = this.ingredientsList.getItems();
+			  ingredients.sort(new IngredientNameComparator());
+			  this.ingredientsList.setItems(ingredients);
+			  this.ingredientsList.refresh();
+		  } else if (this.sortComboBox.getValue().toString().equals("Type")) {
+			  ObservableList<Ingredient> ingredients = this.ingredientsList.getItems();
+			  ingredients.sort(new IngredientTypeComparator());
+			  this.ingredientsList.setItems(ingredients);
+			  this.ingredientsList.refresh();
+		  }
+	  }
+	  
 	@FXML
 	void initialize() {
 		this.ingredientType.getItems().add("Vegetable");
@@ -48,6 +74,9 @@ public class MainWindow {
 		this.ingredientType.getItems().add("Bread");
 		this.ingredientType.getItems().add("Fruit");
 		this.ingredientType.getItems().add("Spice");
-
+		
+		this.sortComboBox.getItems().add(new IngredientNameComparator());
+		this.sortComboBox.getItems().add(new IngredientTypeComparator());
+		this.sortComboBox.setValue(this.sortComboBox.getItems().get(0));
 	}
 }
