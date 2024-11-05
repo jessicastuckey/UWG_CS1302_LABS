c638084bfee4550f89842bed2edd0e48dec797f1
Jessica G Stuckey <js00547@my.westga.edu>
2024-11-05 18:43:42 -0500

    Added Test Class
    Added test class for viewmodel.

---
+++ code/Lab 7/src/edu/westga/cs1302/password_generator/test/viewmodel/TestGeneratePassword.java
@@ -0,0 +1,32 @@
+package edu.westga.cs1302.password_generator.test.viewmodel;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+import edu.westga.cs1302.password_generator.viewmodel.PasswordGeneratorViewModel;
+
+class TestGeneratePassword {
+
+	@Test
+	public void testWhenMinLengthIsInvalid() {
+		PasswordGeneratorViewModel generator = new PasswordGeneratorViewModel();
+		generator.getMinLengthProperty().setValue("a");
+		assertThrows(NumberFormatException.class, ()->{generator.generatePassword();});
+	}
+
+	@Test
+	public void testWhenMinLengthLessThanOne() {
+		PasswordGeneratorViewModel generator = new PasswordGeneratorViewModel();
+		generator.getMinLengthProperty().setValue("0");
+		assertThrows(IllegalArgumentException.class, ()->{generator.generatePassword();});
+	}
+
+	@Test
+	public void testNoErrors() {
+		PasswordGeneratorViewModel generator = new PasswordGeneratorViewModel();
+		generator.getMinLengthProperty().setValue("5");
+		String password = generator.generatePassword();
+		assertTrue(5 <= password.length());
+	}
+}
