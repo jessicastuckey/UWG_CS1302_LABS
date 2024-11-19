3daaf5c12816213f6b346bc7938415d64461d85c
Jessica G Stuckey <js00547@my.westga.edu>
2024-11-19 15:52:24 -0500

    Implimented Feature 1
    Added a menu bar with Close and About MenuItems. Added code to display a popup with information on About and to close the window on Close.

--- code/Lab 9/Lab 9/src/edu/westga/cs1302/password_generator/view/MainWindow.java
+++ code/Lab 9/Lab 9/src/edu/westga/cs1302/password_generator/view/MainWindow.java
@@ -1,12 +1,17 @@
 package edu.westga.cs1302.password_generator.view;
 
 import edu.westga.cs1302.password_generator.viewmodel.ViewModel;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.Button;
 import javafx.scene.control.CheckBox;
 import javafx.scene.control.Label;
+import javafx.scene.control.MenuItem;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
 
 /** Codebehind for the MainWindow of the Application.
  * 
@@ -22,8 +27,25 @@ public class MainWindow {
     @FXML private TextArea output;
     @FXML private Label errorTextLabel;
     @FXML private Button generatePasswordButton;
+    @FXML private MenuItem aboutMenuItem;
+    @FXML private MenuItem closeMenuItem;
+    @FXML private AnchorPane anchorPane;
     
     private ViewModel vm;
+    
+    @FXML
+    void aboutOnClick(ActionEvent event) {
+    	Alert informationPopup = new Alert(AlertType.INFORMATION);
+    	informationPopup.setTitle("About");
+    	informationPopup.setHeaderText("Project Information:");
+    	informationPopup.setContentText("Password Generator generates a list of passwords according to the given properties. Author: Jessica Stuckey");
+    	informationPopup.showAndWait();
+    }
+
+    @FXML
+    void closeOnClick(ActionEvent event) {
+    	this.generatePasswordButton.getParent().getScene().getWindow().hide();
+    }
     
     @FXML
     void initialize() {
